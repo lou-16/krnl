c8cc67ada0af94afedacf96fe8548ff00bb3691c
lou-16 <gurnoor16singh@gmail.com>
2024-11-12 21:54:05 +0530

    been a while lol

--- libc/string/string.c
+++ libc/string/string.c
@@ -1,4 +1,5 @@
-int strlen(char* str){
+#include "../include/string.h"
+int strlen(const char* str){
     char c = str[0];
     int i = 0; 
     while (c != '\0')
@@ -7,5 +8,23 @@ int strlen(char* str){
         c = str[i];
     }
     return i;
+    
+}
+
+int memcmp(const void* v1,const void* v2, size_t s){
+    char* _v1, _v2;
+    _v1 = (char*)v1;
+    _v2 = (char*)v2;
+    for(size_t i = 0; i < s; i++){
+        if( _v1 + i == _v2 + i){
+            continue;
+        }
+        else{
+            return (sizeof(_v1) - sizeof(_v2));
+        }
+    }
+}
+
+void* memcpy(void* __restrict var1, const void* __restrict var2, size_t s){
     
 }
