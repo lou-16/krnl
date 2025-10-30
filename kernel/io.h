c0b5c47c4355f518b47998395bf7a1f14b599fbd
lou <114510230+lou-16@users.noreply.github.com>
2025-10-30 23:51:54 +0530

    created io.h, removed inb and outb from serial.h

---
+++ kernel/io.h
@@ -0,0 +1,22 @@
+#ifndef IO_H
+#define IO_H
+
+#include <stdint.h>
+
+static inline void outb(uint16_t port, uint8_t val) {
+    asm volatile ("outb %0, %1" : : "a"(val), "Nd"(port));
+}
+
+static inline uint8_t inb(uint16_t port) {
+    uint8_t ret;
+    asm volatile ("inb %1, %0" : "=a"(ret) : "Nd"(port));
+    return ret;
+}
+
+// TODO : this is a very simple wait. implement a proper sleep
+static inline void io_wait()
+{
+    outb(0x80, 0);
+}
+
+#endif
--- kernel/serial.h
+++ kernel/serial.h
@@ -4,17 +4,9 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#include "io.h"
+
 #define COM1 0x3F8
-
-static inline void outb(uint16_t port, uint8_t val) {
-    asm volatile ("outb %0, %1" : : "a"(val), "Nd"(port));
-}
-
-static inline uint8_t inb(uint16_t port) {
-    uint8_t ret;
-    asm volatile ("inb %1, %0" : "=a"(ret) : "Nd"(port));
-    return ret;
-}
 
 static inline void serial_init() {
     outb(COM1 + 1, 0x00); // Disable interrupts
