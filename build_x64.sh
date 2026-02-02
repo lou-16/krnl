d3e5fa8d052a6e1fa7afa8dcba5fb990daf23e26
Gurnoor Singh <114510230+lou-16@users.noreply.github.com>
2026-02-02 22:48:40 +0530

    Refactor build_x64.sh for improved structure and clarity

--- build_x64.sh
+++ build_x64.sh
@@ -1,65 +1,57 @@
-#!/bin/bash 
+#!/bin/bash
+set -e
 
-# deps: git, make
-echo "Checking if Limine Exists..."
-cd limine
-if [[ $? != 0 ]]; then
-    echo "Limine not found. peforming a git operation"
-    cd ..
-    git clone https://codeberg.org/Limine/Limine.git limine
-    echo "Limine cloned. building ..."
-    cd Limine
-    make 
-    echo "built limine"
-    
+SCRIPT_DIR="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
+echo "Checking if Limine exists..."
+
+if [[ ! -d "$SCRIPT_DIR/limine" ]]; then
+    echo "Limine not found. Cloning..."
+    git clone https://codeberg.org/Limine/Limine.git "$SCRIPT_DIR/limine" \
+        --branch=v10.x-binary --depth=1
+
+    echo "Building Limine..."
+    make -C "$SCRIPT_DIR/limine"
 fi
-cd ..
-# back to root
-mkdir -p build
-echo "Running CMake"
-cd build
-cmake ..
 
-echo "Moving kernel.elf to iso"
+echo "Running CMake..."
+rm -rf "$SCRIPT_DIR/build"
+mkdir -p "$SCRIPT_DIR/build"
+cd "$SCRIPT_DIR/build"
+cmake .. -DCMAKE_C_COMPILER=x86_64-elf-gcc
+make
 
-cd ..
-#back to root
-mkdir -p iso
-cp ./build/kernel.elf ./iso/
+echo "Preparing ISO tree..."
+mkdir -p "$SCRIPT_DIR/iso/BOOT"
+cp "$SCRIPT_DIR/build/kernel.elf" "$SCRIPT_DIR/iso/"
 
-echo "moving Limine files to iso folder"
-cp ./limine/limine-bios-cd.bin ./iso
-cp ./limine/limine-bios.sys ./iso
-cp ./limine/limine-uefi-cd.bin ./iso
-cd iso
-mkdir -p BOOT
-cd ..
-#back to root
-cp ./limine/BOOTX64.EFI ./iso/BOOT
-echo "moved Limine files to iso folder"
+cp "$SCRIPT_DIR/limine/limine-bios-cd.bin" "$SCRIPT_DIR/iso/"
+cp "$SCRIPT_DIR/limine/limine-bios.sys" "$SCRIPT_DIR/iso/"
+cp "$SCRIPT_DIR/limine/limine-uefi-cd.bin" "$SCRIPT_DIR/iso/"
+cp "$SCRIPT_DIR/limine/BOOTX64.EFI" "$SCRIPT_DIR/iso/BOOT/"
 
-echo "creating limine.conf"
-cat ./iso/BOOT/limine.conf << EOF
+echo "Creating limine.conf..."
+cat << EOF > "$SCRIPT_DIR/iso/limine.conf"
 TIMEOUT=5
 
 /krnl-x86_64
     protocol: limine
-    kernel_path: boot():/kernel.elf" 
+    kernel_path: boot():/kernel.elf
 EOF
 
-echo "checking if the tree path is fine PLEASE VERIFY"
-tree ./iso
-
-echo "building ISO"
-xorriso -as mkisofs -R -J -b limine-bios-cd.bin \
+echo "Building ISO..."
+xorriso -as mkisofs \
+    -R -J \
+    -b limine-bios-cd.bin \
     -no-emul-boot \
     -boot-load-size 4 \
     -boot-info-table \
     --efi-boot limine-uefi-cd.bin \
     --efi-boot-part --efi-boot-image \
-    -o limine.iso ./iso
-echo "Running limine bios-install"
+    -o "$SCRIPT_DIR/krnl-x86_64.iso" \
+    "$SCRIPT_DIR/iso"
 
-./limine/limine bios-install limine.iso
+echo "Running limine bios-install..."
+"$SCRIPT_DIR/limine/limine" bios-install "$SCRIPT_DIR/limine.iso"
 
-echo "ISO prepared"
+echo "ISO prepared."
