2182455b2ed79dab35c0e155bc7430a502ea90bd
Gurnoor <gurnoor16singh@gmail.com>
2026-08-07 00:37:36 +0530

    fix: updated .gitignore

---
+++ kernel/arch/i386/init.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <stddef.h>
+
+// for now, 2 are fine, im gonna support one anyway lol
+enum CPU_ARCH {
+    x86 = 1,
+    x64 = 2
+};
+
+typedef size_t MAIN_MEMORY_LENGTH;
+typedef int CPU_CORES;
+
+typedef struct kInfo_t {
+
+} kInfo_t;
